03c34c0195481ba6d33667cf5ae0df8fe3487575
dasarisiddhu <siddhudasari11@gmail.com>
2025-08-31 10:29:34 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,9 @@
-# questions-on-python
+"""Reverse a string in Python using slicing."""
+s = "rajesh"
+o = ''.join(reversed(s))
+print(o)
 
+"""Reverse a string in Python using slicing."""
+s = "rajesh"
+o = s[:: -1]
+print(o)
